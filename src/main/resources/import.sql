7557f7fb09d3c8533e1303f3b40d02b01b34a81c
Tsung-Hsiang Wu <aplmnbvcxz@gmail.com>
2021-05-29 22:21:10 +0800

    [DEV] corrected typo

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -20,6 +20,6 @@ INSERT INTO Ingredient(id, name, type) VALUES ('SRCR', 'Sour Cream', 4);
 /* Insert Users */
 INSERT INTO Users(username, password, enabled) VALUES ('admin', 'sysadmin', true);
 INSERT INTO Users(username, password, enabled) VALUES ('user', 'test', true);
-/* Insert Users' Authoritues */
+/* Insert UserAuthorities */
 INSERT INTO UserAuthorities(username, authority) VALUES ('admin', 'admin');
 INSERT INTO UserAuthorities(username, authority) VALUES ('user', 'customer');
